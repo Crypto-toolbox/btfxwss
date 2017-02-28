0991151d7b93ac38a027427f7fa75963cb5136b6
nlsdfnbch <23okrs35+github@mykolab.com>
2017-02-28 13:00:52 +0100

    added dependencies to project

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from distutils.core import setup
 setup(name='btfxwss', version='0.1', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitfinex_wss",
-      packages=['btfxwss'])
+      packages=['btfxwss'], install_requires=['websocket-client'])
