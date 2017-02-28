86d26649f0f3b62d4033b3a22fc266a12849796f
nlsdfnbch <23okrs35+github@mykolab.com>
2017-02-28 13:05:15 +0100

    added licence

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='btfxwss', version='0.1', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
-      url="https://github.com/nlsdfnbch/bitfinex_wss",
+      url="https://github.com/nlsdfnbch/bitfinex_wss", license='MIT',
       packages=['btfxwss'], install_requires=['websocket-client'])
