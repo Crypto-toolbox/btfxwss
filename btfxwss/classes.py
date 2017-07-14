4ea08421494ff5c1b530914aa0897546a0a008f8
nlsdfnbch <23okrs35+github@mykolab.com>
2017-07-14 15:10:25 +0200

    removed BtfxWssRaw; updated subscribe methods

--- btfxwss/classes.py
+++ btfxwss/classes.py
@@ -93,12 +93,15 @@ class BtfxWss:
         self.secret = secret if secret else ''
 
         # Set up book-keeping variables & configurations
-        self.channel_configs = {}  # Variables, as set by subscribe command
+        self.channel_configs = defaultdict(dict)  # Variables, as set by subscribe command
+
+        self.channel_directory = {}
 
         self.q = queue.Queue()
         self.conn = WebSocketConnection(self.q, log_level=log_level,
                                         **wss_kwargs)
-        self.queue_processor = QueueProcessor(self.q, log_level=log_level)
+        self.queue_processor = QueueProcessor(self.q, self.channel_directory,
+                                              log_level=log_level)
 
     @property
     def tickers(self):
@@ -132,23 +135,6 @@ class BtfxWss:
         self.conn.disconnect()
         self.queue_processor.join()
 
-    ##
-    # Response Message Handlers
-    ##
-
-
-    ##
-    # Commands
-    ##
-
-    def ping(self):
-        """
-        Pings Websocket server to check if it's still alive.
-        :return:
-        """
-        self.ping_timer = time.time()
-        self.conn.send(json.dumps({'event': 'ping'}))
-
     def config(self, decimals_as_strings=True, ts_as_dates=False,
                sequencing=False, **kwargs):
         """
@@ -168,30 +154,22 @@ class BtfxWss:
             flags += 65536
         q = {'event': 'conf', 'flags': flags}
         q.update(kwargs)
-        self.conn.send(json.dumps(q))
+        self.conn.send(**q)
 
-    def _subscribe(self, channel_name, **kwargs):
-        if not self.conn:
-            log.error("_subscribe(): Cannot subscribe to channel,"
-                      "since the client has not been started!")
-            return
+    def _subscribe(self, channel_name, identifier, **kwargs):
         q = {'event': 'subscribe', 'channel': channel_name}
         q.update(**kwargs)
         log.debug("_subscribe: %s", q)
-        self.conn.send(json.dumps(q))
+        self.conn.send(**q)
+        self.channel_configs[identifier] = q
 
-    def _unsubscribe(self, channel_name, **kwargs):
-        if not self.conn:
-            log.error("_unsubscribe(): Cannot unsubscribe from channel,"
-                      "since the client has not been started!")
-            return
-        try:
-            chan_id = self.channels.pop(channel_name)
-        except KeyError:
-            raise NotRegisteredError("_unsubscribe(): %s" % channel_name)
-        q = {'event': 'unsubscribe', 'chanId': chan_id}
+    def _unsubscribe(self, channel_name, identifier, **kwargs):
+
+        channel_id = self.channel_directory[(channel_name,)]
+        q = {'event': 'unsubscribe', 'chanId': channel_id}
         q.update(kwargs)
-        self.conn.send(json.dumps(q))
+        self.conn.send(**q)
+        self.channel_configs.pop(identifier)
 
     def ticker(self, pair, unsubscribe=False, **kwargs):
         """
@@ -200,10 +178,11 @@ class BtfxWss:
         :param kwargs:
         :return:
         """
+        identifier = ('ticker', pair)
         if unsubscribe:
-            self._unsubscribe('ticker', symbol=pair, **kwargs)
+            self._unsubscribe('ticker', identifier, symbol=pair, **kwargs)
         else:
-            self._subscribe('ticker', symbol=pair, **kwargs)
+            self._subscribe('ticker', identifier, symbol=pair, **kwargs)
 
     def order_book(self, pair, unsubscribe=False, **kwargs):
         """
@@ -212,10 +191,11 @@ class BtfxWss:
         :param kwargs:
         :return:
         """
+        identifier = ('book', pair)
         if unsubscribe:
-            self._unsubscribe('book', symbol=pair, **kwargs)
+            self._unsubscribe('book', identifier, symbol=pair, **kwargs)
         else:
-            self._subscribe('book', symbol=pair, **kwargs)
+            self._subscribe('book', identifier, symbol=pair, **kwargs)
 
     def raw_order_book(self, pair, prec=None, unsubscribe=False, **kwargs):
         """
@@ -224,11 +204,12 @@ class BtfxWss:
         :param kwargs:
         :return:
         """
+        identifier = ('raw_book', pair)
         prec = 'R0' if prec is None else prec
         if unsubscribe:
-            self._unsubscribe('book', pair=pair, prec=prec, **kwargs)
+            self._unsubscribe('book', identifier, pair=pair, prec=prec, **kwargs)
         else:
-            self._subscribe('book', pair=pair, prec=prec, **kwargs)
+            self._subscribe('book', identifier, pair=pair, prec=prec, **kwargs)
 
     def trades(self, pair, unsubscribe=False, **kwargs):
         """
@@ -237,12 +218,13 @@ class BtfxWss:
         :param kwargs:
         :return:
         """
+        identifier = ('trades'. pair)
         if unsubscribe:
-            self._unsubscribe('trades', symbol=pair, **kwargs)
+            self._unsubscribe('trades', identifier, symbol=pair, **kwargs)
         else:
-            self._subscribe('trades', symbol=pair, **kwargs)
+            self._subscribe('trades', identifier, symbol=pair, **kwargs)
 
-    def ohlc(self, pair, timeframe=None, unsubcribe=False, **kwargs):
+    def candles(self, pair, timeframe=None, unsubcribe=False, **kwargs):
         """
         Subscribe to the passed pair's OHLC data channel.
         :param pair: str, Pair to request data for.
@@ -251,6 +233,7 @@ class BtfxWss:
         :param kwargs:
         :return:
         """
+        identifier = ('candled', pair, timeframe)
         valid_tfs = ['1m', '5m', '15m', '30m', '1h', '3h', '6h', '12h', '1D',
                      '7D', '14D', '1M']
         if timeframe:
@@ -261,9 +244,9 @@ class BtfxWss:
         pair = 't' + pair if not pair.startswith('t') else pair
         key = 'trade:' + timeframe + ':' + pair
         if unsubcribe:
-            self._unsubscribe('candles', key=key, **kwargs)
+            self._unsubscribe('candles', identifier, key=key, **kwargs)
         else:
-            self._subscribe('candles', key=key, **kwargs)
+            self._subscribe('candles', identifier, key=key, **kwargs)
 
     ##
     # Private Endpoints
@@ -287,239 +270,3 @@ class BtfxWss:
     def unauth(self):
         js = {'event': 'unauth', 'chanId': 0}
         self.conn.send(json.dumps(js))
-
-class BtfxWssRaw(BtfxWss):
-    """
-    Bitfinex Websocket API Client. Inherits from BtfxWss, but stores data in raw
-    format on disk instead of in-memory. Rotates files every 24 hours by default.
-    """
-
-    def __init__(self, key=None, secret=None, addr=None, output_dir=None,
-                 rotate_after=None, rotate_at=None, rotate_to=None):
-        """
-        Initializes BtfxWssRaw Instance.
-        :param key: Api Key as string.
-        :param secret: Api secret as string.
-        :param addr: Websocket API Address.
-        :param output_dir: Directory to create file descriptors in.
-        :param rotate_after: time in seconds, after which descriptor ought to be
-                             rotated. Default 3600*24s.
-        :param rotate_at: optional time, date str in format 'HH:MM' (24h clock),
-                          at which descriptors should be rotated. Mutually
-                          exclusive with rotate_after.
-        :param rotate_to: path as str, target folder to copy data to.
-        """
-        super(BtfxWssRaw, self).__init__(key=key, secret=secret, addr=addr)
-
-        self.tar_dir = output_dir if output_dir else '/tmp/'
-
-        if not rotate_after and not rotate_at:
-            self.rotate_after = rotate_after if rotate_after else 3600 * 24
-            self.rotate_at = None
-        elif rotate_after and rotate_at:
-            raise ValueError("Can only specify either rotate_at or rotate_after! Not both!")
-        else:
-            self.rotate_after = rotate_after
-            self.rotate_at = rotate_at
-
-        self.rotate_to = rotate_to if rotate_to else '/var/tmp/data/'
-
-        # File Desciptor Variables
-        self.tickers = None
-        self.books = None
-        self.raw_books = None
-        self._trades = None
-        self.candles = None
-        self.account = None
-
-        self.rotator_thread = None
-
-    def _init_file_descriptors(self, path=None):
-        """
-        Assign the file descriptors to their respective attributes.
-        :param path: str, defaults to self.tar_dir
-        :return:
-        """
-        path = path if path else self.tar_dir
-        self.tickers = open(path + 'btfx_tickers.csv', 'a', encoding='UTF-8')
-        self.books = open(path + 'btfx_books.csv', 'a', encoding='UTF-8')
-        self.raw_books = open(path + 'btfx_rawbooks.csv', 'a', encoding='UTF-8')
-        self._trades = open(path + 'btfx_trades.csv', 'a', encoding='UTF-8')
-        self.candles = open(path + 'btfx_candles.csv', 'a', encoding='UTF-8')
-        if self.key and self.secret:
-            self.account = open(path + 'btfx_account.csv', 'a', encoding='UTF-8')
-
-    def _close_file_descriptors(self):
-        """
-        Closes all file descriptors
-        :return:
-        """
-        self.tickers.close()
-        self.books.close()
-        self.raw_books.close()
-        self._trades.close()
-        self.candles.close()
-        if self.account:
-            self.account.close()
-
-    def _rotate(self):
-        """
-        Function for the rotator thread, which calls the rotate_descriptors()
-        method after the set interval stated in self.rotate_after
-        :return:
-        """
-        t = time.time()
-        rotated = False
-        while self.running:
-
-            time_to_rotate = ((time.time() - t >= self.rotate_after)
-                              if self.rotate_after
-                              else (datetime.datetime.now().strftime('%H:%M') ==
-                                    self.rotate_at))
-
-            if time_to_rotate and not rotated:
-                self._rotate_descriptors(self.rotate_to)
-                rotated = True
-                t = time.time()
-            else:
-                rotated = False
-                time.sleep(1)
-
-    def _rotate_descriptors(self, target_dir):
-        """
-        Acquires the processor lock and cCloses file descriptors, renames the
-        files and moves them to target_dir.
-        Afterwards, opens new batch of file descriptors and releases the lock.
-        :param target_dir: str, path.
-        :return:
-        """
-        with self._processor_lock:
-
-            # close file descriptors
-            self._close_file_descriptors()
-
-            # Move old files to a new location
-            fnames = ['btfx_tickers.csv', 'btfx_books.csv', 'btfx_rawbooks.csv',
-                      'btfx_candles.csv', 'btfx_trades.csv']
-            if self.account:
-                fnames.append('btfx_account.csv')
-            date = time.strftime('%Y-%m-%d_%H:%M:%S')
-            for fname in fnames:
-                ex_name, dtype = fname.split('_')
-                new_name = ex_name + '_' + date + '_' + dtype
-                src = self.tar_dir+'/'+fname
-                tar = target_dir + '/' + new_name
-                shutil.copy(src, tar)
-                os.remove(src)
-
-            # re-open file descriptors
-            self._init_file_descriptors()
-
-    def start(self):
-        """
-        Start the websocket client threads
-        :return:
-        """
-        self._init_file_descriptors()
-
-        super(BtfxWssRaw, self).start()
-
-        log.info("BtfxWss.start(): Initializing rotator thread..")
-        if not self.rotator_thread:
-            self.rotator_thread = Thread(target=self._rotate, name='Rotator Thread')
-            self.rotator_thread.start()
-        else:
-            log.info("BtfxWss.start(): Thread not started! "
-                     "self.rotator_thread is populated!")
-
-    def stop(self):
-        """
-        Stop all threads and modules of the client.
-        :return:
-        """
-        super(BtfxWssRaw, self).stop()
-
-        log.info("BtfxWssRaw.stop(): Joining rotator thread..")
-        try:
-            self.rotator_thread.join()
-            if self.rotator_thread.is_alive():
-                time.time(1)
-        except AttributeError:
-            log.debug("BtfxWss.stop(): Rotator thread was not running!")
-
-        self.rotator_thread = None
-
-        log.info("BtfxWssRaw.stop(): Done!")
-
-    ##
-    # Data Message Handlers
-    ##
-
-    def _handle_ticker(self, ts, chan_id, data):
-        """
-        Adds received ticker data to self.tickers dict, filed under its channel
-        id.
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: tuple or list of data received via wss
-        :return:
-        """
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self.tickers.write(js + '\n')
-
-    def _handle_book(self, ts, chan_id, data):
-        """
-        Updates the order book stored in self.books[chan_id]
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: dict, tuple or list of data received via wss
-        :return:
-        """
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self.books.write(js + '\n')
-
-    def _handle_raw_book(self, ts, chan_id, data):
-        """
-        Updates the raw order books stored in self.raw_books[chan_id]
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: dict, tuple or list of data received via wss
-        :return:
-        """
-
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self.raw_books.write(js + '\n')
-
-    def _handle_trades(self, ts, chan_id, data):
-        """
-        Files trades in self._trades[chan_id]
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: list of data received via wss
-        :return:
-        """
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self._trades.write(js + '\n')
-
-    def _handle_candles(self, ts, chan_id, data):
-        """
-        Stores OHLC data received via wss in self.candles as json
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: list of data received via wss
-        :return:
-        """
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self.candles.write(js + '\n')
-
-    def _handle_auth(self, ts, chan_id, data):
-        """
-        Store Account specific data received by authenticating to the API in
-        self.account, as json.
-        :param ts: timestamp, declares when data was received by the client
-        :param chan_id: int, channel id
-        :param data: list of data received via wss
-        :return:
-        """
-        js = json.dumps((ts, self.channel_labels[chan_id], data))
-        self.account.write(js + '\n')
