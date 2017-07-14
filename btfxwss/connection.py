d6d8a8c64485e93a37849ac65a9d607df0ab7eef
nlsdfnbch <23okrs35+github@mykolab.com>
2017-07-14 13:15:49 +0200

    added to docstring, added missing response channels

--- btfxwss/connection.py
+++ btfxwss/connection.py
@@ -305,13 +305,15 @@ class WebSocketConnection(Thread):
             self._info_handler(data)
         elif event == 'error':
             self._error_handler(data)
-        elif event in ('subscribe', 'unsubscribe', 'conf'):
+        elif event in ('subscribe', 'unsubscribe', 'conf', 'auth', 'unauth'):
             self._response_handler(event, data, ts)
         else:
             self.log.error("Unhandled event: %s, data: %s", event, data)
 
     def _response_handler(self, event, data, ts):
-        """Handles responses to (un)subscribe and conf commands
+        """Handles responses to (un)subscribe and conf commands.
+
+        Passes data up to client.
 
         :param data:
         :param ts:
