e8a01e764e2312658df762ead17c1b9d2343b19c
nlsdfnbch <23okrs20+github@mykolab.com>
2017-09-10 12:56:49 +0200

    add check for connected state and log Error in send() #44

--- btfxwss/connection.py
+++ btfxwss/connection.py
@@ -254,6 +254,9 @@ class WebSocketConnection(Thread):
         :param kwargs: payload paarameters as key=value pairs
         :return:
         """
+        if not self.connected.is_set():
+            self.log.error("send(): Did not send out payload %s - client not connected. ", kwargs)
+            return
         if list_data:
             payload = json.dumps(list_data)
         else:
