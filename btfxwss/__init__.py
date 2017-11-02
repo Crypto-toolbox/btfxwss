bf78181bdde776c95dab06424738d69b54198e6e
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-02 10:33:00 +0100

    updated __init__s

--- btfxwss/__init__.py
+++ btfxwss/__init__.py
@@ -1,3 +1,5 @@
 from btfxwss.client import BtfxWss
+from btfxwss.zmq import WebSocketConnection
+from btfxwss.zmq import BtfxWssSocketProcess, BtfxWssSocketThread, BtfxWssSocketProcess
 
 
---
+++ btfxwss/zmq/__init__.py
@@ -0,0 +1,2 @@
+from btfxwss.zmq.connection import WebSocketConnection
+from btfxwss.zmq.sockets import BtfxWssSocket, BtfxWssSocketThread, BtfxWssSocketProcess
