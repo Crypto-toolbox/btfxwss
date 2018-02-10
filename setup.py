040b2ffda026933da3cb90f4e8a9d1130fde76a5
Nils Diefenbach <nils.diefenbach@celeraone.com>
2018-02-10 11:34:26 +0100

    Update Setup and requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
-setup(name='btfxwss', version='1.1.3', author='Nils Diefenbach',
+setup(name='btfxwss', version='1.2.0', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitfinex_wss", license='LICENCSE',
-      packages=['btfxwss'], install_requires=['websocket-client'],
+      packages=['btfxwss', 'btfxwss.zmq'], install_requires=['websocket-client'],
       description="Python 3.5+ Websocket Client for the Bitfinex WSS API.")
