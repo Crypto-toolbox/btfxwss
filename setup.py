f01b0e13b91d2d0c54a99f7a8a7ef0126bf8cf3e
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-10-19 22:13:48 +0200

    Update email, license.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='btfxwss', version='1.2.2', author='Nils Diefenbach',
-      author_email='23okrs20+pypi@mykolab.com',
-      url="https://github.com/nlsdfnbch/bitfinex_wss", license='LICENCSE',
+      author_email='nlsdfnbch.foss@kolabnow.com',
+      url="https://github.com/nlsdfnbch/bitfinex_wss", license='MIT',
       packages=['btfxwss'], install_requires=['websocket-client'],
       description="Python 3.5+ Websocket Client for the Bitfinex WSS API.")
