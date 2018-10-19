429ce6919f2cabe8bd9647d98649e541f53db213
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-10-19 22:09:22 +0200

    Bump version -> 1.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-setup(name='btfxwss', version='1.2.0b5', author='Nils Diefenbach',
+setup(name='btfxwss', version='1.2.2', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitfinex_wss", license='LICENCSE',
       packages=['btfxwss'], install_requires=['websocket-client'],
